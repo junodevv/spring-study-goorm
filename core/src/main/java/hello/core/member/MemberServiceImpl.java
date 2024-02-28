5020b49645ebaf5b061a0189eafb44702ce8faaa
leejuno <zhqkfxm0127@naver.com>
2024-02-28 18:57:21 +0900

    feat: AppConfig를 통한 관심사 분리, 의존관계주입(DI)

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,7 +3,12 @@ package hello.core.member;
 // 구현체가 하나인 경우 클래스명 끝에 "Impl"을 붙이는 관례가 있다.
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+//    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,17 +2,31 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
-
+    /*
     private final MemberRepository memberRepository = new MemoryMemberRepository();
     private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    */
+    //위 두 코드 할인정책의 변경을 위해서 클라이언트인 현재 클래스를 변경해야하는 문제 발생, DIP, OCP위반
+
+    /** DIP, OCP 지키는 코드
+     * 여기서(클라이언트)는 인터페이스에만 의존하고, 어떤 할인정책, DB를 사용하는지는 AppConfig(생성자를 호출하는쪽에서 결정)
+     * */
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
-
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findMyId(memberId);
         // 아래코드가 단일책임원칙을 잘 지킨것, 할인에대해 변경이 필요해지면 할인쪽만 바꾸면됨
