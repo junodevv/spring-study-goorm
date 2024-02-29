f592fe6e9425464efce5bd327d39a9ba8a707db7
leejuno <zhqkfxm0127@naver.com>
2024-02-29 18:17:20 +0900

    feat: AppConfig 리팩토링 후 정책 변경 시뮬레이션

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -30,7 +31,7 @@ public class AppConfig {
     }
     // 할인정책은 fix할인정책을 쓸 것이다.
     private DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
-
 }
