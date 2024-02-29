af1d4850c83873c8dabcd4d9a2ba13114e905d7f
leejuno <zhqkfxm0127@naver.com>
2024-02-29 18:44:48 +0900

    feat: 스프링 적용, 로그출력 xml파일 추가

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -9,15 +9,19 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration // 설정 정보란 뜻의 어노테이션
 public class AppConfig {
 
     // 생성자를 통해서 객체가 들어감 == 생성자 주입 == DI,Dependency Injection == 의존관계 주입
     // MemoryMemberRepository를 갖고 있는 MemberServiceImpl객체 반환
+    @Bean // 스프링 컨테이너 라는 곳에 등록되는 아이들
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
-
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
@@ -26,12 +30,38 @@ public class AppConfig {
      * 역할과 구현 클래스를 한 눈에 볼 수 있게 된다.
      * */
     // 멤버 리포지토리는 메모리멤버레포를 쓸것이다.(역할을 한눈에 볼 수 있도록 리팩토링한 코드)
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
     // 할인정책은 fix할인정책을 쓸 것이다.
-    private DiscountPolicy discountPolicy() {
+    @Bean
+    public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
 }
+/*스프링 적용 전 코드*/
+/*
+// 생성자를 통해서 객체가 들어감 == 생성자 주입 == DI,Dependency Injection == 의존관계 주입
+// MemoryMemberRepository를 갖고 있는 MemberServiceImpl객체 반환
+public MemberService memberService(){
+    return new MemberServiceImpl(memberRepository());
+}
+public OrderService orderService(){
+    return new OrderServiceImpl(memberRepository(), discountPolicy());
+}
+
+//아래처럼 작성하면 정책이 바뀌어도 아래에서만 바꿔주면되고
+// 역할과 구현 클래스를 한 눈에 볼 수 있게 된다.
+
+// 멤버 리포지토리는 메모리멤버레포를 쓸것이다.(역할을 한눈에 볼 수 있도록 리팩토링한 코드)
+public MemberRepository memberRepository() {
+    return new MemoryMemberRepository();
+}
+// 할인정책은 fix할인정책을 쓸 것이다.
+public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+    return new RateDiscountPolicy();
+}
+*/
--- core/src/main/java/hello/core/OrderApp.java
+++ core/src/main/java/hello/core/OrderApp.java
@@ -7,15 +7,23 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
 //        MemberService memberService = new MemberServiceImpl(null);
 //        OrderService orderService = new OrderServiceImpl(null,null);
-        
+        ////////순수자바///////
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+        ///////스프링////////
+
+        ApplicationContext ap = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ap.getBean("memberService", MemberService.class);
+        OrderService orderService = ap.getBean("orderService", OrderService.class);
+
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
