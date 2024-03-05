cebdfcddd3215bdff6f202a20be190fb711df607
leejuno <zhqkfxm0127@naver.com>
2024-03-05 18:02:09 +0900

    test:스프링 컨테이너를 활용한 싱글톤 코드

--- core/src/test/java/hello/core/singleton/SingletonTest.java
+++ core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -7,12 +7,14 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletonTest {
 
     @Test
     @DisplayName("스프링 없는 순수한 DI 컨테이너")
-    void pureContainer(){
+    void pureContainer() {
         AppConfig appConfig = new AppConfig();
         // 1. 조회: 호출할 때 마다 객체를 생성
         MemberService memberService1 = appConfig.memberService();
@@ -30,7 +32,7 @@ public class SingletonTest {
     // 싱글톤 테스트
     @Test
     @DisplayName("싱글톤 패턴을 적용한 객체 사용")
-    void singletonServiceTest(){
+    void singletonServiceTest() {
         SingletonService singletonService1 = SingletonService.getInstance();
         SingletonService singletonService2 = SingletonService.getInstance();
 
@@ -40,5 +42,22 @@ public class SingletonTest {
         assertThat(singletonService1).isSameAs(singletonService2);
         // same == (객체 비교)
         // equals (값 비교)
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 실글톤")
+    void springContainer(){
+//        AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        // 참조값이 같음
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberService1 == memberService2
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
