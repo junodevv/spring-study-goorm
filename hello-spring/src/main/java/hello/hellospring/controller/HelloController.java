044929d68bfdf794e97558101e9b7e93849cff62
leejuno <zhqkfxm0127@naver.com>
2024-03-07 12:33:27 +0900

    feat(hello-spring): API방식 실습

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -5,19 +5,47 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
     @GetMapping("hello") // "/hello"가 들어오면 이 메서드에 매핑해준다.
-    public String hello(Model model){
+    public String hello(Model model) {
         model.addAttribute("data", "hello!!(${data}에 들어갈 값)");
         return "hello";
     }
 
     @GetMapping("hello-mvc")
-    public String helloMvc(@RequestParam("name") String name, Model model){
+    public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-templete";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // http에서 바디 공간에 이 데이터를 넣어서 보내겠다., json으로 보내준다.
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody // @ResponseBody를 붙이고 객체를 반환하면 JSON으로 반환됨
+    public Hello helloApi(@RequestParam("name")String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
