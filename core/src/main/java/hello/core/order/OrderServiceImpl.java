d251ce1cf467a0b5da947b6b8bd4d911f5fb1a91
leejuno <zhqkfxm0127@naver.com>
2024-03-11 12:28:43 +0900

    feat: 같은 타입의 빈이 2개 이상인 경우 실습

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -8,15 +8,20 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor // final이 붙은 필드(필수값들)를 가지고 생성자를 만든다. 생성자 주입할떄 사용하는 생성자를 똑같이 만들어줌
+//@RequiredArgsConstructor // final이 붙은 필드(필수값들)를 가지고 생성자를 만든다. 생성자 주입할떄 사용하는 생성자를 똑같이 만들어줌
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findMyId(memberId);
