f89dca3a25180d77da7d77fc09b47877a72b9f8f
leejuno <zhqkfxm0127@naver.com>
2024-03-15 14:36:40 +0900

    feat: 웹(request)스코프 활용 1. Proxy

--- core/src/main/java/hello/core/web/LogDemoController.java
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -13,14 +13,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
-    // 이렇게 하면 MyLogger를 주입받는 것이 아닌 mylogger를 Lookup할 수 있는 provider를 주입 받는다.
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody           // httpRequest에 의 한 정보를 받을 수 있음, 클라이언트의 요청정보
     public String logDemo(HttpServletRequest request) throws InterruptedException {
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject(); // myLogger가 호출되는 시점
+
+        System.out.println("myLogger = " + myLogger.getClass());
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
