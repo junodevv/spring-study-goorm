eb1a5bbbb0ee2396bce40ce34ece3db8787c38c8
leejuno <zhqkfxm0127@naver.com>
2024-03-26 12:47:34 +0900

    chore: MVC모델 틀 구현

---
+++ student/src/main/java/com/example/student/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.example.student.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class StudentRepository {
+
+}
---
+++ student/src/main/java/com/example/student/service/StudentService.java
@@ -0,0 +1,7 @@
+package com.example.student.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+}
