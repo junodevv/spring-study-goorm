2f5eb5218d734bd9e2ad2cd8dd0d39b5425ddf4b
leejuno <zhqkfxm0127@naver.com>
2024-03-26 16:25:00 +0900

    feat(service): StudentService 구현

--- student/src/main/java/com/example/student/service/StudentService.java
+++ student/src/main/java/com/example/student/service/StudentService.java
@@ -1,7 +1,26 @@
 package com.example.student.service;
 
+import com.example.student.model.Student;
+import com.example.student.repository.StudentRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class StudentService {
+
+    private final StudentRepository repository;
+
+    public void save(String name, int grade){
+        repository.save(name, grade);
+    }
+
+    public List<Student> getAll (){
+        return repository.findAll();
+    }
+
+    public List<Student> getAllByGrade(int grade){
+        return repository.findByGrade(grade);
+    }
 }
