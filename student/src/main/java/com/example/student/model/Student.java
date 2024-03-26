f7bfd1fe0027a5ff8bbc238d61a3e231ef2dce2a
leejuno <zhqkfxm0127@naver.com>
2024-03-26 15:40:17 +0900

    feat(model): 기본 데이터 반환 메소드 구현(DB대용)

--- student/src/main/java/com/example/student/model/Student.java
+++ student/src/main/java/com/example/student/model/Student.java
@@ -1,11 +1,16 @@
 package com.example.student.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 @AllArgsConstructor
 public class Student implements Comparable<Student>{
     private String name;
@@ -15,4 +20,12 @@ public class Student implements Comparable<Student>{
     public int compareTo(Student s) {
         return this.grade - s.getGrade(); // 메서드를 호출하는 객체가 비교대상보다 작은 경우 음수 반환 (오름차순)
     }
+
+    public static List<Student> defaultStudent(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("kim", 1));
+        students.add(new Student("lee", 3));
+        return students;
+    }
+
 }
