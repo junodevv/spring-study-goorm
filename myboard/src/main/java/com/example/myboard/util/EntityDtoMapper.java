eef8c62e78fc0c800da18e6302cc442b1c826547
leejuno <zhqkfxm0127@naver.com>
2024-04-05 11:00:27 +0900

    docs: 배운것들

--- myboard/src/main/java/com/example/myboard/util/EntityDtoMapper.java
+++ myboard/src/main/java/com/example/myboard/util/EntityDtoMapper.java
@@ -5,7 +5,7 @@ import com.example.myboard.model.entity.Board;
 
 public class EntityDtoMapper {
     // 본문 미 포함된 응답
-    public static BoardDto mapBoardToDto(Board board){
+    public static BoardDto mapBoardToDtoNoContent(Board board){
         return BoardDto.builder()
                 .boardNo(board.getBoardNo())
                 .title(board.getTitle())
